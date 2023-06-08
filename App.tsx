09ae6a071d07a4498bcd01301c55aea76eb38d08
Jeferson Mesquita <je_2742@hotmail.com>
2023-06-07 21:32:37 -0300

    Image (Core Component)

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Profissional React Native</Text>
+      <View style={{ backgroundColor: "red" }}>
+        <Image
+          onLoad={({ nativeEvent }) => {
+            console.log("width: ", nativeEvent.source.width);
+            console.log("height: ", nativeEvent.source.height);
+          }}
+          resizeMode="contain"
+          source={{ uri: "https://images.unsplash.com/photo-1661956602153-23384936a1d3" }}
+          style={{ height: 300, width: 300 }}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
