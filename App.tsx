b1010f656a9969769ee7fc30bab959f21b4234ff
Jeferson Mesquita <je_2742@hotmail.com>
2023-06-20 08:03:48 -0300

    Button, Touchable e Pressable (Core Component)

--- App.tsx
+++ App.tsx
@@ -1,36 +1,69 @@
-import { StyleSheet, Text, View, Image, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  Alert,
+  Platform,
+  TouchableHighlight,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
 
 export default function App() {
+  const changeName = (text: string) => {
+    console.log(text);
+  };
+
+  function onPressButton() {
+    console.log("Botão pressionado");
+    Alert.alert("Meu primeiro app", Platform.OS);
+  }
+
   return (
     <View style={styles.container}>
-      <Text 
+      <Text
         style={{
           fontSize: 25,
           fontWeight: "bold",
         }}
-      >Profissional React Native</Text>
+      >
+        Profissional React Native
+      </Text>
       <TextInput
         autoCapitalize="none"
         multiline
         placeholder="Digite seu nome"
-        // onChange={(e) => {
-        //   console.log(e.nativeEvent.text);
-        // }}
-        onChangeText={(text) => {
-          console.log(text);
-        }}
+        onChangeText={changeName}
         placeholderTextColor="#ddd"
-        style={{
-          paddingHorizontal	: 10,
-          borderWidth: 1,
-          height: 50,
-          width: 250,
-          marginTop: 10,
-          color: "#550ab1",
-          fontWeight: "bold",
-          fontSize: 18,
-        }}
+        style={styles.textInput}
       />
+      <TouchableHighlight
+        onPressIn={() => console.log("onPressIn")}
+        underlayColor={"#550ab1"}
+        style={styles.button}
+        onPress={() => console.log("Hightlight pressionado")}
+      >
+        <Text style={styles.buttonTitle}>Highlight</Text>
+      </TouchableHighlight>
+      <TouchableOpacity
+        onPressOut={() => console.log("onPressOut")}
+        activeOpacity={0.7}
+        style={styles.button}
+        onPress={() => console.log("Opacity pressionado")}
+      >
+        <Text style={styles.buttonTitle}>Opacity</Text>
+      </TouchableOpacity>
+      <Pressable
+        delayLongPress={1000}
+        onLongPress={() => console.log("onLongPress")}
+        style={styles.button}
+        onPress={() => console.log("pressable pressionado")}
+      >
+        <Text style={styles.buttonTitle}>Pressable</Text>
+      </Pressable>
     </View>
   );
 }
@@ -41,5 +74,29 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  textInput: {
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    height: 50,
+    width: 250,
+    marginTop: 10,
+    color: "#550ab1",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  button: {
+    height: 50,
+    width: 200,
+    backgroundColor: "#1D013F",
+    borderRadius: 12,
+    marginTop: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
   },
 });
