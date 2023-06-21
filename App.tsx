4119013bed7aa7c48ab6a639de66978c0af1507e
Jeferson Mesquita <je_2742@hotmail.com>
2023-06-21 08:24:19 -0300

    Conheça o Flexbox

--- App.tsx
+++ App.tsx
@@ -1,61 +1,34 @@
-import {
-  View,
-  StyleSheet,
-  Text,
-  Alert,
-  Platform,
-  ScrollView,
-  FlatList,
-  ListRenderItemInfo,
-} from "react-native";
-import { Button } from "./components/Button";
-import { TextInput } from "./components/TextInput";
-import { List } from "./components/List";
-import { login } from "./service/authService";
-import { Coffee, coffeeList } from "./CoffeeList/data/coffeeList";
-import { CoffeeItem } from "./CoffeeList/CoffeeItem";
-import { coffeeList100 } from "./CoffeeList/data/cofeeLis100";
-import { useState } from "react";
-import { CoffeeHeader } from "./CoffeeList/CoffeeHeader";
-import { SeparatorItem } from "./CoffeeList/SeparatorItem";
+import { View } from "react-native";
 
 export default function App() {
-  const changeName = (text: string) => {
-    console.log(text);
-  };
-
-  async function onPressButton() {
-    try {
-      const token = await login("je_2742@hotmail.com", "123456");
-      Alert.alert("Sucesso", `Token: ${token}`);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  function renderItem({ item }: ListRenderItemInfo<Coffee>) {
-    return <CoffeeItem {...item} />;
-  }
-
   return (
-    <View style={styles.container}>
-      <FlatList
-        ListHeaderComponent={<CoffeeHeader />}
-        ItemSeparatorComponent={SeparatorItem}
-        data={coffeeList}
-        keyExtractor={(coffee) => coffee.name}
-        renderItem={renderItem}
-        // renderItem={({ item, index }) => <CoffeeItem {...item} />}
-        // initialNumToRender={10}
-        // windowSize={3}
+    <View
+      style={{
+        backgroundColor: "#EEE",
+        flex: 1,
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+        }}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
